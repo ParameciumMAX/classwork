8d0c7a8014eb47b7cb0ee5cb05c40e0a6ba280a1
ParameciumMAX <a41923960@gmail.com>
2024-10-25 14:05:45 +0800

    20241025 edited

--- life_of_game.js
+++ life_of_game.js
@@ -1,8 +1,8 @@
 var abc=[
     [0,0,0,0,0], // 0
-    [0,1,0,1,0], // 1
-    [0,0,1,0,0], // 2
-    [0,1,0,1,0], // 3
+    [0,1,0,0,0], // 1
+    [0,0,0,0,0], // 2
+    [0,0,0,0,0], // 3
     [0,0,0,0,0]   //4
 ];
 
@@ -25,17 +25,17 @@ for(var a2=0;a2<row;a2++){
     }
 };
 
-function find(i,j){
+/*function find(i,j){
     for(var x = -1;x<=1;x++){
         for(var y = -1;y<=1;y++){
     if((i+x)<0||(j+x)<0){
         return 0;
-    }else{
+    }else if(abc[i][j]==1){
         counta+=1;
     }
     }
 }
-};
+};*/
 
 console.log("There are "+count+" number 1.");
 console.log("There are "+counta+" life.");
